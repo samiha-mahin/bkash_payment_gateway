148a0fdc03af20901f3a5a29893c6b416f1fbb32
samiha-mahin <samihamahin36@gmail.com>
2025-07-10 03:47:39 +0600

    bkash

--- server/index.js
+++ server/index.js
@@ -1,3 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+
+dotenv.config({});
 
 const app = express();
+
+//middlewares
+app.use(express.json()); //Parses JSON payloads from request body.
+app.use(express.urlencoded({extended:true})); //Parses URL-encoded payloads (e.g., forms).
+app.use(bodyParser.json())
+
+const corsOptions = {
+    origin: ' http://localhost:5173',
+  credentials: true, // Ensures cookies are sent
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
