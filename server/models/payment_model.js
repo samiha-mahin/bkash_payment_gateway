43438400e2491f5b959ca43bea8c217ca811a2a1
samiha-mahin <samihamahin36@gmail.com>
2025-07-10 17:10:56 +0600

    bkash

---
+++ server/models/payment_model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const payment = new Schema({
+    userId: {
+        type: Number,
+    },
+    amount: {
+        type: Number,
+    },
+    trxID: {
+        type: String,
+    },
+    paymentID: {
+        type: String,
+    },
+    date: {
+        type: String,
+    }
+}, { timestamps: true })
+
+module.exports = model('payments', payment)
