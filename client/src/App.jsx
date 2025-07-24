7599baa571a11b2da7a2ccb856e5908bd5c861b6
samiha-mahin <samihamahin36@gmail.com>
2025-07-24 22:50:19 +0600

    bkash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Error from './components/Error'
+import Success from './components/Success'
 
-const App = () => {
+
+function App() {
+
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/error?' element={<Error />} />
+          <Route path='/success' element={<Success />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
