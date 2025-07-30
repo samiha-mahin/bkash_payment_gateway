815b9787a393b752aacb56c0d714b588a2deb35d
samiha-mahin <samihamahin36@gmail.com>
2025-07-30 23:16:05 +0600

    bkash

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import Success from './Success'
+import axios from 'axios'
 
 const Home = () => {
-  return (
-    <div><Success/>
-    </div>
-  )
+
+    const pay = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:4000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
+            window.location.href = data.bkashURL
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+    return (
+        <div>
+            <button onClick={pay}>Pay bkash</button>
+        </div>
+    )
 }
 
 export default Home
