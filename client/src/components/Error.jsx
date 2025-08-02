44d1eb0e6872fe2fda55c6b1336640e40be0b99d
samiha-mahin <samihamahin36@gmail.com>
2025-08-02 23:27:52 +0600

    bkash

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Error = () => {
-  return (
-    <div>Error</div>
-  )
+    const searchData = new URLSearchParams(window.location.search)
+    const message = searchData.get('message')
+    return (
+        <div>payment {message}</div>
+    )
 }
 
 export default Error
